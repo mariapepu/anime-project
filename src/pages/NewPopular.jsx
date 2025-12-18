90f8356ee307a00423926327e3c86886839a3d56
mariapepu <mperezpulles@gmail.com>
2025-12-18 07:41:05 +0100

    feat: use global trending logic in New & Popular page

--- src/pages/NewPopular.jsx
+++ src/pages/NewPopular.jsx
@@ -6,7 +6,7 @@ import { useAnime } from '../context/AnimeContext';
 
 const NewPopular = () => {
     const [playingAnime, setPlayingAnime] = useState(null);
-    const { animeList, loading } = useAnime();
+    const { animeList, trendingList, loading } = useAnime();
 
     if (loading) return <div className="text-white pt-20 pl-[4%]">Loading...</div>;
 
@@ -18,7 +18,7 @@ const NewPopular = () => {
             <div className='w-full text-white pt-[100px]'>
                 <h1 className='text-3xl font-bold mb-4 px-[4%]'>New & Popular</h1>
                 <Row title="New Releases" animes={newReleases} onPlay={setPlayingAnime} />
-                <Row title="Trending Now" animes={animeList} onPlay={setPlayingAnime} />
+                <Row title="Trending Now" animes={trendingList} onPlay={setPlayingAnime} />
             </div>
             {playingAnime && (
                 <Player anime={playingAnime} onClose={() => setPlayingAnime(null)} />
