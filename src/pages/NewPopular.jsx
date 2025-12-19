9c76a908bf43513a078e5d684e027032260fd537
mariapepu <mperezpulles@gmail.com>
2025-12-19 02:30:23 +0100

    Sync New & Popular with Home page New Releases

--- src/pages/NewPopular.jsx
+++ src/pages/NewPopular.jsx
@@ -6,18 +6,16 @@ import { useAnime } from '../context/AnimeContext';
 
 const NewPopular = () => {
     const [playingAnime, setPlayingAnime] = useState(null);
-    const { animeList, trendingList, loading } = useAnime();
+    const { animeList, trendingList, newReleasesList, loading } = useAnime();
 
     if (loading) return <div className="text-white pt-20 pl-[4%]">Loading...</div>;
-
-    const newReleases = animeList.filter(anime => anime.isNew);
 
     return (
         <>
             <Navbar />
             <div className='w-full text-white pt-[100px]'>
                 <h1 className='text-3xl font-bold mb-4 px-[4%]'>New & Popular</h1>
-                <Row title="New Releases" animes={newReleases} onPlay={setPlayingAnime} />
+                <Row title="New Releases" animes={newReleasesList} onPlay={setPlayingAnime} />
                 <Row title="Trending Now" animes={trendingList} onPlay={setPlayingAnime} />
             </div>
             {playingAnime && (
